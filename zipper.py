472ac5ff7640a1f556f41de05a83569b60169e9c
Jonathan Farinloye <farinloyejonathan@gmail.com>
2019-06-09 18:18:55 +0100

    :boom: :boom: Still broken compression formats added

--- zipper.py
+++ zipper.py
@@ -6,8 +6,9 @@ from zipfile import PyZipFile
 
 formats = list(i[0] for i in shutil.get_archive_formats())
 form = list(i[0] for i in formats)
-
 # TODO more than zip
+
+
 def unzip():
     for compressed in sys.argv[2:]:
         if FileNotFoundError:
@@ -16,12 +17,17 @@ def unzip():
             PyZipFile(compressed).extractall()
 
 
-def zip():
-    for name in sys.argv[2:]:
-        if FileNotFoundError:
-            print('No file or folder named: ' + name)
-        else:
-            shutil.make_archive(name, 'zip', root_dir=None, base_dir=name)
+def compress():
+    if sys.argv[2] not in form:
+        print('Bros small small')
+    else:
+        archive = formats[form.index(sys.argv[2])]
+        for name in sys.argv[3:]:
+            if FileNotFoundError:
+                print(sys.argv)
+                print('No file or folder named: ' + name)
+            else:
+                shutil.make_archive(name, str(archive), root_dir=None, base_dir=name)
 
 
 try:
@@ -33,7 +39,7 @@ try:
             print('    ' + form[i] + '  -  ' + formats[i])
     else:
         if sys.argv[1] == 'zip':
-            zip()
+            compress()
         elif sys.argv[1] == 'unzip':
             unzip()
 except Exception as e:
