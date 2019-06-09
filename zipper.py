6e66834272da2cfdfdaa86fde1683a09eb3da49b
Jonathan Farinloye <farinloyejonathan@gmail.com>
2019-06-09 22:30:11 +0100

    :sparkles: added format for uncompression

--- zipper.py
+++ zipper.py
@@ -3,7 +3,8 @@
 import shutil
 import sys
 import os
-from zipfile import PyZipFile
+import zipfile
+import tarfile
 
 formats = list(i[0] for i in shutil.get_archive_formats())
 form = list(i[0] for i in formats)
@@ -11,11 +12,14 @@ form = list(i[0] for i in formats)
 
 
 def unzip():
-    for compressed in sys.argv[2:]:
-        if FileNotFoundError:
-            print('No file or folder named: ' + compressed)
-        else:
-            PyZipFile(compressed).extractall()
+    for compressed in sys.argv[3:]:
+        try:
+            if os.path.exists(compressed):
+                shutil.unpack_archive(compressed)
+            else:
+                print('No file or folder named: ' + compressed)
+        except ValueError:
+            print('We might get there one day. But for now, Unsupported archive format')
 
 
 def compress():
@@ -40,13 +44,15 @@ try:
     if len(sys.argv) <= 3:
         print('Zipper by Jonathan Farinloye version 1.4')
         print('/usr/bin/zipper')
-        print('Usage: zipper [zip|unzip] ' + str(form[0:]).replace('\'', '').replace(', ', '|') + ' [file|folder]')
+        print('Usage: zipper [up|down] ' + str(form[0:]).replace('\'', '').replace(', ', '|') + ' [file|folder]')
         for i in range(len(form)):
             print('    ' + form[i] + '  -  ' + formats[i])
+        print('up   -   Compress a file or directory')
+        print('down -   Extract the contents of a compressed folder')
     else:
-        if sys.argv[1] == 'zip':
+        if sys.argv[1] == 'up':
             compress()
-        elif sys.argv[1] == 'unzip':
+        elif sys.argv[1] == 'down':
             unzip()
 except Exception as e:
     print(e)
